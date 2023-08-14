36178fe439e38aad7a75f76ed011c80974ce0b2e
ClementBerthier <berthierclementpro@gmail.com>
2023-08-14 12:40:42 +0200

    cinematic burger menu ok

--- src/components/Mobile/BurgerMenu/BurgerMenu.jsx
+++ src/components/Mobile/BurgerMenu/BurgerMenu.jsx
@@ -1,11 +1,26 @@
+import { useState } from "react";
 import "./BurgerMenu.scss";
 
 function BurgerMenu() {
+    const [isMenuClicked, setIsMenuClicked] = useState(false);
+    const [burgerBarClass, setBurgerBarClass] = useState(
+        "burger-bar unclicked"
+    );
+
+    const handleClick = () => {
+        setIsMenuClicked(!isMenuClicked);
+        setBurgerBarClass("burger-bar clicked");
+
+        isMenuClicked
+            ? setBurgerBarClass("burger-bar unclicked")
+            : setBurgerBarClass("burger-bar clicked");
+    };
+
     return (
-        <div className="burger-menu">
-            <div className="burger-bar"></div>
-            <div className="burger-bar"></div>
-            <div className="burger-bar"></div>
+        <div className="burger-menu" onClick={handleClick}>
+            <div className={burgerBarClass}></div>
+            <div className={burgerBarClass}></div>
+            <div className={burgerBarClass}></div>
         </div>
     );
 }
