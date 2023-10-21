e77ed2f61c2c84d3882c8488e2de46a27ad070b1
ClementBerthier <berthierclementpro@gmail.com>
2023-10-21 13:36:36 +0200

    test safeareaview

--- src/index.jsx
+++ src/index.jsx
@@ -3,9 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./components/Common/App.jsx";
 import "../src/reset.scss";
 import "../src/index.scss";
+import { SafeAreaView } from "react-native";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <App />
+        <SafeAreaView>
+            <App />
+        </SafeAreaView>
     </React.StrictMode>
 );
