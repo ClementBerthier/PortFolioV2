7d82f5299d63c0aea3a98806b1a7a3d1092a9ed9
ClementBerthier <berthierclementpro@gmail.com>
2023-10-21 15:29:02 +0200

    add manifest for PWA

--- src/components/Common/App.jsx
+++ src/components/Common/App.jsx
@@ -2,11 +2,26 @@ import "./App.scss";
 import useMobileDetect from "../../Hook/useMobileDetect.js";
 import HomeMobile from "../Mobile/Home/HomeMobile.jsx";
 import HomeDesktop from "../Desktop/Home/HomeDesktop.jsx";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
     const isMobile = useMobileDetect();
 
-    return <>{isMobile ? <HomeMobile /> : <HomeDesktop />}</>;
+    return (
+        <>
+            {isMobile ? (
+                <Routes>
+                    {" "}
+                    <Route path="/" element={<HomeMobile />} />{" "}
+                </Routes>
+            ) : (
+                <Routes>
+                    {" "}
+                    <Route path="/" element={<HomeDesktop />} />{" "}
+                </Routes>
+            )}
+        </>
+    );
 }
 
 export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -3,9 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./components/Common/App.jsx";
 import "../src/reset.scss";
 import "../src/index.scss";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+    <BrowserRouter>
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    </BrowserRouter>
 );
