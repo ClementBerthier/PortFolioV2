657ebbc3b72d64c04674c1d3a5a8c2a34e83cc8f
ClementBerthier <berthierclementpro@gmail.com>
2023-10-21 13:42:36 +0200

    test safeareaview

--- src/components/Common/App.jsx
+++ src/components/Common/App.jsx
@@ -2,16 +2,11 @@ import "./App.scss";
 import useMobileDetect from "../../Hook/useMobileDetect.js";
 import HomeMobile from "../Mobile/Home/HomeMobile.jsx";
 import HomeDesktop from "../Desktop/Home/HomeDesktop.jsx";
-import { SafeAreaView } from "react-native";
 
 function App() {
     const isMobile = useMobileDetect();
 
-    return (
-        <SafeAreaView>
-            {isMobile ? <HomeMobile /> : <HomeDesktop />}
-        </SafeAreaView>
-    );
+    return <>{isMobile ? <HomeMobile /> : <HomeDesktop />}</>;
 }
 
 export default App;
