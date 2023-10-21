a04f9a72328c25000473e0c2f979a2bf7ac65c53
ClementBerthier <berthierclementpro@gmail.com>
2023-10-21 15:56:34 +0200

    ajust value of svg

--- src/components/Mobile/BurgerButton/BurgerButton.jsx
+++ src/components/Mobile/BurgerButton/BurgerButton.jsx
@@ -13,17 +13,17 @@ function BurgerButton() {
     const svgValues1 = [
         100, 99.89, 99.875, 99.84, 99.72, 99.61, 99.44, 99.27, 97.79, 96.31,
         90.28, 84.25, 72.12, 60, 46.87, 33.75, 25.54, 17.33, 12.25, 7.17, 5.5,
-        3.84, 2.7, 1.56, 1.17, 0.78, 0.55, 0.33, 0.23, 0.13, 0.06, 0.03, -1,
+        3.84, 2.7, 1.56, 1.17, 0.78, 0.55, 0.33, 0.23, 0.13, 0.06, 0.03, 0,
     ];
     const svgValues2 = [
         100, 93.47, 90.2, 86.94, 82.66, 78.39, 74.11, 69.84, 62.16, 54.48, 46.8,
         39.12, 35.89, 32.66, 29.43, 26.21, 23.54, 20.88, 18.22, 15.56, 13.57,
-        11.58, 9.59, 7.61, 6.29, 4.98, 3.66, 2.35, 1.76, 1.17, 0.58, 0.29, -1,
+        11.58, 9.59, 7.61, 6.29, 4.98, 3.66, 2.35, 1.76, 1.17, 0.58, 0.29, 0,
     ];
 
     const [counter, setCounter] = useState(0);
 
-    const svgPath = `M 0 0 H ${svgValues1[counter]}C ${svgValues1[counter]} 12.5 ${svgValues2[counter]} 12.5 ${svgValues2[counter]} 25 C ${svgValues2[counter]} 37.5 ${svgValues1[counter]} 37.5 ${svgValues1[counter]} 50 C ${svgValues1[counter]} 62.5 ${svgValues2[counter]} 62.5 ${svgValues2[counter]} 75 C ${svgValues2[counter]} 87.5 ${svgValues1[counter]} 87.5 ${svgValues1[counter]} 100 H 100 V 0`;
+    const svgPath = `M 0 0 H ${svgValues1[counter]}C ${svgValues1[counter]} 12.5 ${svgValues2[counter]} 12.5 ${svgValues2[counter]} 25 C ${svgValues2[counter]} 37.5 ${svgValues1[counter]} 37.5 ${svgValues1[counter]} 50 C ${svgValues1[counter]} 62.5 ${svgValues2[counter]} 62.5 ${svgValues2[counter]} 75 C ${svgValues2[counter]} 87.5 ${svgValues1[counter]} 87.5 ${svgValues1[counter]} 100 H 100 V -1`;
 
     const Counter = () => {
         if (burgerMenuClass === "burger-menu clicked") {
