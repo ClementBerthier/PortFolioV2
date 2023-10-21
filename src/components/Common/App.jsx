f8e6f433b28a08bcc1bf876ceddef4d2b4812942
ClementBerthier <berthierclementpro@gmail.com>
2023-10-21 13:40:26 +0200

    test safeareaview

--- src/components/Common/App.jsx
+++ src/components/Common/App.jsx
@@ -2,11 +2,16 @@ import "./App.scss";
 import useMobileDetect from "../../Hook/useMobileDetect.js";
 import HomeMobile from "../Mobile/Home/HomeMobile.jsx";
 import HomeDesktop from "../Desktop/Home/HomeDesktop.jsx";
+import { SafeAreaView } from "react-native";
 
 function App() {
     const isMobile = useMobileDetect();
 
-    return <>{isMobile ? <HomeMobile /> : <HomeDesktop />}</>;
+    return (
+        <SafeAreaView>
+            {isMobile ? <HomeMobile /> : <HomeDesktop />}
+        </SafeAreaView>
+    );
 }
 
 export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -3,12 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./components/Common/App.jsx";
 import "../src/reset.scss";
 import "../src/index.scss";
-import { SafeAreaView } from "react-native";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <SafeAreaView>
-            <App />
-        </SafeAreaView>
+        <App />
     </React.StrictMode>
 );
